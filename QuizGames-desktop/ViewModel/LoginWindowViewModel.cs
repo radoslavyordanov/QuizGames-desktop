8c886144326aab80037ef3090e037397ccea7ff4
Radoslav Yordanov <radoslav@kodovpotok.bg>
2016-05-02 13:33:47 +0300

    ADd view model logic.

--- QuizGames-desktop/ViewModel/LoginWindowViewModel.cs
+++ QuizGames-desktop/ViewModel/LoginWindowViewModel.cs
@@ -4,11 +4,19 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QuizGames_desktop.ViewModel {
-    class LoginWindowViewModel: INotifyPropertyChanged {
+    class LoginWindowViewModel : INotifyPropertyChanged {
         //This event will be fired to notify any listeners of a change in a property value.
         public event PropertyChangedEventHandler PropertyChanged;
+        private string usernameTB = "";
+        private string passwordTB = "";
+        private ICommand loginCommand;
+        public LoginWindowViewModel() {
+            loginCommand = new RelayCommand(onLoginClick);
+        }
+
         //Tell WPF Binding that this property value has changed
         public void PropChanged(String propertyName) {
             //Did WPF registerd to listen to this event
@@ -19,10 +27,41 @@ namespace QuizGames_desktop.ViewModel {
         }
 
 
+        public void onLoginClick(object obj) {
+             System.Windows.MessageBox.Show(usernameTB + " " + passwordTB);
+        }
 
+ 
+        public string UsernameTB {
+            get {
+                return usernameTB;
+            }
+            set {
+                usernameTB = value;
+                PropChanged("UsernameTB");
+            }
+        }
 
+        
+        public string PasswordTB {
+            get {
+                return passwordTB;
+            }
+            set {
+                passwordTB = value;
+                PropChanged("PasswordTB");
+            }
+        }
 
-
+        
+        public ICommand LoginCommand {
+            get {
+                return loginCommand;
+            }
+            set {
+                loginCommand = value;
+            }
+        }
 
 
     }
