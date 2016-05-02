0f3fb97093f7fdd86b33007e98d311e77d78a3a4
Radoslav Yordanov <radoslav@kodovpotok.bg>
2016-05-02 12:56:11 +0300

    Added WPF project to GIT.

---
+++ QuizGames-desktop/ViewModel/LoginWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGames_desktop.ViewModel {
+    class LoginWindowViewModel: INotifyPropertyChanged {
+        //This event will be fired to notify any listeners of a change in a property value.
+        public event PropertyChangedEventHandler PropertyChanged;
+        //Tell WPF Binding that this property value has changed
+        public void PropChanged(String propertyName) {
+            //Did WPF registerd to listen to this event
+            if (PropertyChanged != null) {
+                //Tell WPF that this property changed
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
